91e6d310694aa1db319cc754a8f88cdb99cb019b
Koya <koyavsaito@gmail.com>
2019-11-30 20:24:44 -0500

    alight title

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ var button = document.getElementById("toggle_blur");
 var blur = document.getElementById("text_input_container");
 
 function doMe() {
-    blur.style.blur("0px");
+    blur.style.backgroundColor = "#fa4";
 }
 
 
