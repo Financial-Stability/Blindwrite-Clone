d7763af6064d48bda2a62c66081945ab596cdc29
Koya <koyavsaito@gmail.com>
2019-11-30 20:28:17 -0500

    alight title

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,15 @@
 var button = document.getElementById("toggle_blur");
 var blur = document.getElementById("text_input_container");
+var isBlurred = true;
 
 function doMe() {
-    blur.style.WebkitFilter = "blur(0px)";
+    if (isBlurred) {
+        blur.style.WebkitFilter = "blur(0px)";
+        isBlurred = false;
+    } else {
+        blur.style.WebkitFilter = "blur(10px)";
+        isBlurred = true;
+    }
 }
 
 
