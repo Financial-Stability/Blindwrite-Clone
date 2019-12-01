c22c6792936bb03afa558ee2195e739adc588849
Koya <koyavsaito@gmail.com>
2019-11-30 20:51:01 -0500

    alight title

--- JS/script.js
+++ JS/script.js
@@ -18,5 +18,21 @@ function toggleBlur() {
 button.onclick = toggleBlur;
 
 // For timer
-
-window.setTimeout
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("progressBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
